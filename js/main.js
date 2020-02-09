d8fbae3abff7c469f3e8b34926f1b8dd0b78fd5d
damiandominella <damiandominella@gmail.com>
2020-02-09 01:40:19 +0100

    late night edits

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
 function onThemeToggle(theme) {
-    document.body.className = theme;
+  document.body.className = theme;
 }
+
+function updateScroll() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("progress-bar").style.width = scrolled + "%";
+}
+
+window.onscroll = function() {
+  updateScroll();
+};
