0208ff6ef9a2e8b306b80183257c2a07f56faf1d
damiandominella <damiandominella@gmail.com>
2020-03-22 19:55:45 +0100

    sunday things

--- js/main.js
+++ js/main.js
@@ -14,3 +14,24 @@ function updateScroll() {
 window.onscroll = function() {
   updateScroll();
 };
+
+// Load github repos
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.open('GET', 'https://api.github.com/users/damiandominella/repos', true);
+xmlhttp.onreadystatechange = function() {
+    if (xmlhttp.readyState == 4) {
+        if(xmlhttp.status == 200) {
+            var data = JSON.parse(xmlhttp.responseText);
+            var html = "<ul>";
+
+            for (item of data) {
+              html += `<li><a href=${item.html_url} target="_blank"><span class="name">${item.name}</span><span class="desc">${item.description}</span></a></li>`
+            }
+
+            html += "</ul>";
+
+            document.getElementById("github-repositories").innerHTML = html;
+         }
+    }
+};
+xmlhttp.send(null);
