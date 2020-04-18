ee373e332cd7f63d53130d82cb5bd81c027ba7a2
damiandominella <damiandominella@gmail.com>
2020-04-18 02:19:36 +0200

    friday night quarantine boring things

--- js/main.js
+++ js/main.js
@@ -2,6 +2,11 @@ function onThemeToggle(theme) {
   document.body.className = theme;
 }
 
+function onColorToggle(hex) {
+  var root = document.documentElement;
+  root.style.setProperty("--accent", hex);
+}
+
 function updateScroll() {
   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
   var height =
@@ -11,27 +16,27 @@ function updateScroll() {
   document.getElementById("progress-bar").style.width = scrolled + "%";
 }
 
-window.onscroll = function() {
+window.onscroll = function () {
   updateScroll();
 };
 
 // Load github repos
 var xmlhttp = new XMLHttpRequest();
-xmlhttp.open('GET', 'https://api.github.com/users/damiandominella/repos', true);
-xmlhttp.onreadystatechange = function() {
-    if (xmlhttp.readyState == 4) {
-        if(xmlhttp.status == 200) {
-            var data = JSON.parse(xmlhttp.responseText);
-            var html = "<ul>";
+xmlhttp.open("GET", "https://api.github.com/users/damiandominella/repos", true);
+xmlhttp.onreadystatechange = function () {
+  if (xmlhttp.readyState == 4) {
+    if (xmlhttp.status == 200) {
+      var data = JSON.parse(xmlhttp.responseText);
+      var html = "<ul>";
 
-            for (item of data) {
-              html += `<li><a href=${item.html_url} target="_blank"><span class="name">${item.name}</span><span class="desc">${item.description}</span></a></li>`
-            }
+      for (item of data) {
+        html += `<li><a href=${item.html_url} target="_blank"><span class="name">${item.name}</span><span class="desc">${item.description}</span></a></li>`;
+      }
 
-            html += "</ul>";
+      html += "</ul>";
 
-            document.getElementById("github-repositories").innerHTML = html;
-         }
+      document.getElementById("github-repositories").innerHTML = html;
     }
+  }
 };
 xmlhttp.send(null);
