7362e57a9360f7839b07cd53a18e84a0f63d921f
damiandominella <damiandominella@gmail.com>
2020-09-22 22:25:47 +0200

    fix

--- js/main.js
+++ js/main.js
@@ -1,42 +1,43 @@
 function onThemeToggle(theme) {
-  document.body.className = theme;
+    document.body.className = theme
 }
 
 function onColorToggle(hex) {
-  var root = document.documentElement;
-  root.style.setProperty("--accent", hex);
+    var root = document.documentElement
+    root.style.setProperty('--accent', hex)
 }
 
 function updateScroll() {
-  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  var height =
-    document.documentElement.scrollHeight -
-    document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 100;
-  document.getElementById("progress-bar").style.width = scrolled + "%";
+    var winScroll =
+        document.body.scrollTop || document.documentElement.scrollTop
+    var height =
+        document.documentElement.scrollHeight -
+        document.documentElement.clientHeight
+    var scrolled = (winScroll / height) * 100
+    document.getElementById('progress-bar').style.width = scrolled + '%'
 }
 
 window.onscroll = function () {
-  updateScroll();
-};
+    updateScroll()
+}
 
 // Load github repos
-var xmlhttp = new XMLHttpRequest();
-xmlhttp.open("GET", "https://api.github.com/users/damiandominella/repos", true);
+var xmlhttp = new XMLHttpRequest()
+xmlhttp.open('GET', 'https://api.github.com/users/damiandominella/repos', true)
 xmlhttp.onreadystatechange = function () {
-  if (xmlhttp.readyState == 4) {
-    if (xmlhttp.status == 200) {
-      var data = JSON.parse(xmlhttp.responseText);
-      var html = "<ul>";
+    if (xmlhttp.readyState == 4) {
+        if (xmlhttp.status == 200) {
+            var data = JSON.parse(xmlhttp.responseText)
+            var html = '<ul>'
 
-      for (item of data) {
-        html += `<li><a href=${item.html_url} target="_blank"><span class="name">${item.name}</span><span class="desc">${item.description}</span></a></li>`;
-      }
+            for (item of data) {
+                html += `<li><a href=${item.html_url} target="_blank"><span class="name">${item.name}</span><span class="desc">${item.description}</span></a></li>`
+            }
 
-      html += "</ul>";
+            html += '</ul>'
 
-      document.getElementById("github-repositories").innerHTML = html;
+            document.getElementById('github-repositories').innerHTML = html
+        }
     }
-  }
-};
-xmlhttp.send(null);
+}
+xmlhttp.send(null)
